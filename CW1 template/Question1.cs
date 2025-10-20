02adc974ed49b338aa3631b3ab7ced03f3b74ca2
B00971915 <B00971915@MF116PC42>
2025-10-20 09:44:58 +0100

    Add project files.

---
+++ CW1 template/Question1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW1_template
+{
+    public class Question1
+    {
+        public static void question1()
+        {
+            // Question 1: 
+            Console.WriteLine("-------Question 1-------");
+            // Enter code and comments here...
+
+           
+        }
+
+    }
+}
